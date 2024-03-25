131a2e3ce8072f19c68b0478866f4d1d87fcdb78
Mintesinot-Tefera <mintesinotteferamt@gmail.com>
2024-03-25 09:58:10 -0700

    task1edited

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Installs puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => gem,
 }
