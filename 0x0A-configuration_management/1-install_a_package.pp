c2167b34ea2a5073d2e5ec18ca9fc104c9b53b4e
Mintesinot-Tefera <mintesinotteferamt@gmail.com>
2024-03-25 10:14:12 -0700

    task1edited3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# install the package puppet-lint
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
